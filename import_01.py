8866e8481fc72ecf7ae9db78663a357b33e79056
Hung Yu Lin <aa25622515@gmail.com>
2021-12-02 21:39:48 +0800

    final version

--- import_01.py
+++ import_01.py
@@ -1,14 +1,19 @@
 import random  #python
 import os
 
-random_01 = random.randint(1, 100) #rand + int 產生隨機整數 包含頭尾
+
+x = input('請決定開始範圍, 開始值:')
+x = int(x)
+y = input('請決定開始範圍, 結束值:')
+y = int(y)
+random_01 = random.randint(x, y) #rand + int 產生隨機整數 包含頭尾
 print(random_01)
 
 print('---------------------------猜數字遊戲開始------------------------------')
 answer_01 = input('請輸入你要猜的數字(1-100)! 有五次機會喔')
 answer_01 = int(answer_01)
 chances = 5
-while chances > 1:
+while chances >= 1:
 	if answer_01 == random_01:
 		print('好厲害喔猜對了')
 		os._exit() 
